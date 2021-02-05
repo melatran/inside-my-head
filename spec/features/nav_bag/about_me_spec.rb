135ea84b00b97e4ff56cac1a9572d275c00bf0c3
Melanie Tran <melaniexx02@gmail.com>
2021-02-04 18:32:47 -0700

    Migrate files over for inital setup of app

---
+++ spec/features/nav_bag/about_me_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe "User can click on About Me" do
+  scenario "Links to the about me page" do
+    visit "/"
+    click_on "About Me"
+    expect(current_path).to eq("/about")
+  end
+end
+
